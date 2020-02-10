6223e3cf492c37d5935d3d619573b1930d2f0221
matthew nosworthy <40416119@live.napier.ac.uk>
2020-02-10 14:25:05 +0000

    sql database working, second release

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -24,7 +24,7 @@ public class App {
             System.exit(-1);
         }
 
-        int retries = 10;
+        int retries = 5;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
